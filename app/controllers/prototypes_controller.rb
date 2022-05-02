7962caa5b5be259c4d7ef039d04a356a74ed9d04
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-05-02 19:36:42 +0900

    エラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,8 @@
 class PrototypesController < ApplicationController
    before_action :set_prototype, only: [:edit, :show]
    before_action :authenticate_user!, except:[:index, :show]
-   before_action :move_to_index, except: [:index, :show]
+   before_action :move_to_index, only: [:edit, :update, :destroy]
+
    def index
      @prototypes = Prototype.includes(:user)
    end
